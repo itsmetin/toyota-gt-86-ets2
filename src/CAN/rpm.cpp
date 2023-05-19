c7ad18b8062e4f540fee5b3c453f602ac416aa29
Tin <130148056+itsmetin@users.noreply.github.com>
2023-05-19 14:42:39 +0200

    Update rpm.cpp

--- src/CAN/rpm.cpp
+++ src/CAN/rpm.cpp
@@ -1,14 +1,14 @@
 #include "e90canbus.h"
 #include "globals.h"
 
-const uint16_t CAN_ID = 0x0AA;
-uint8_t rpm_frame[8] = {0x5F, 0x59, 0xFF, 0x00, 0x34, 0x0D, 0x80, 0x99};
+const uint16_t CAN_ID = 0x141;
+uint8_t rpm_frame[8] = {0x8F, 0x26, 0xDF, 0x28, 0x34, 0x0D, 0xA7, 0x00};
 
 void canSendRPM(){
-  uint16_t value = s_rpm * 4;
+  uint16_t value = s_rpm / 256;
 
-  rpm_frame[4] = value;
-  rpm_frame[5] = (value >> 8);
+  rpm_frame[5] = value;
+  rpm_frame[6] = value + 0x80;
 
   CAN.sendMsgBuf(CAN_ID, 0, 8, rpm_frame);
 }
